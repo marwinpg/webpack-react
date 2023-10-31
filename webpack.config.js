578c8c5debcd7441a0099d52076d8f3b12655e66
Marwin <m.pinedag2014@gmail.com>
2023-10-31 12:53:50 -0500

    🔧 [ADD] webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+      compress: true,
+      port: 3006,
+      open: true,
+      historyApiFallback: true,
+    },
+  },
+};
